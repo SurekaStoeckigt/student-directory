915a9c13891bf1cb5a037fef5c4c2c06e30790a6
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-01-24 09:56:43 +0000

    asking for cohort input, using symbols and supplying default value

--- directory.rb
+++ directory.rb
@@ -1,4 +1,19 @@
-@center = 50
+@center = 100
+def add_cohort
+cohort_arr = { 1 => :January, 2 => :February, 3 => :March, 4=> :April,
+            5 => :May, 6 => :June, 7 => :July, 8 => :August,
+            9 => :September, 10 => :October, 11 => :November, 12 => :December}
+            puts "Enter number of the month of the cohort to join: ".center(@center)
+            num = gets.chomp.to_i
+            if cohort_arr.has_key?num
+            cohort = cohort_arr[num]
+            else 
+            cohort = cohort_arr[12]
+            end
+            cohort 
+
+end
+
 def input_students
   puts "Please enter the names of the students".center(@center)
   puts "To finish, just hit return twice".center(@center)
@@ -12,19 +27,21 @@ def input_students
   height = gets.chomp
   puts "Now enter a hobby".center(@center)
   hobby = gets.chomp
+  cohort = add_cohort
   while !name.empty? do
   # add the student hash to the array
-    students << {name: name, cohort: :november, country: country, height: height, hobby: hobby}
+    students << {name: name, cohort: cohort, country: country, height: height, hobby: hobby}
     puts "Now we have #{students.count} students".center(@center)
   #get another name from the usesr
     name = gets.chomp
-    puts "Now enter the country of birth".center(@center)
     if !name.empty?
+      puts "Now enter the country of birth".center(@center)
       country = gets.chomp
       puts "Now enter height".center(@center)
       height = gets.chomp
       puts "Now enter a hobby".center(@center)
       hobby = gets.chomp
+      cohort = add_cohort
     else
     end
   end
@@ -76,6 +93,7 @@ end
 
 
 
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(@center)
 end 
@@ -90,3 +108,4 @@ print_footer(students)
 # input_students 
 # line 5: create an empty array to store future students
 # Then ask for the name of the student and discard the return character
+
